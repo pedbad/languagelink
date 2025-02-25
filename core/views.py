d840e46d1fabcc12a0273e4c51e6b126e5e3a030
Pedram Badakhchani <ped@tech3.langcen.private.cam.ac.uk>
2025-02-25 14:47:44 +0000

    Refactored core views.py and added comments.

--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
+# View for the landing page
 def landing_page(request):
-    return render(request, 'core/landing_page.html')
+  return render(request, 'core/landing_page.html')
 
+# View for the about page
 def about_page(request):
-    return render(request, 'core/about.html')
+  return render(request, 'core/about.html')
